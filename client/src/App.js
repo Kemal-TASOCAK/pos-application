97cdca8ee0bffce8a8304ae55446e4cca1ff682a
Kemal TAŞOCAK <59031493+Kemal-TASOCAK@users.noreply.github.com>
2024-03-26 18:40:39 +0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import { Button, Flex } from "antd";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <Button type="primary" danger>
+        Primary
+      </Button>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
