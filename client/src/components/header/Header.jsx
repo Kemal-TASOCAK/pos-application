9856bc9284b9551ae950de64ed2e25a88f353282
Kemal TAŞOCAK <59031493+Kemal-TASOCAK@users.noreply.github.com>
2024-03-28 15:32:08 +0300

    24. Link Kullanımı

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Badge, Input } from "antd";
 import {
   SearchOutlined,
@@ -31,59 +32,59 @@ const Header = () => {
         fixed z-50 bottom-0 md:w-auto w-screen md:bg-transparent bg-white left-0
         md:border-t-0 border-t md:px-0 px-4 py-1"
         >
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <HomeOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Ana Sayfa</span>
-          </a>
+          </Link>
           <Badge count={5} offset={[0, 6]} className="md:flex hidden">
-            <a
-              href={"/"}
+            <Link
+              to={"/cart"}
               className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined className="text-2xl" />
               <span className="md:text-xs text-[10px]">Sepet</span>
-            </a>
+            </Link>
           </Badge>
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <CopyOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Faturalar</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
-          </a>
-          <a
-            href={"/"}
+          </Link>
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <LogoutOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Çıkış</span>
-          </a>
+          </Link>
         </div>
         <Badge count={5} offset={[0, 6]} className="md:hidden flex">
-          <a
-            href={"/"}
+          <Link
+            to={"/"}
             className="menu-link flex flex-col hover:text-[#40a9ff] transition-all"
           >
             <ShoppingCartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Sepet</span>
-          </a>
+          </Link>
         </Badge>
       </header>
     </div>
