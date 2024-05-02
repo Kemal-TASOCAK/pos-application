903bb8854c4eecf656a3d69e41aa982f2e2f4b4d
Kemal TAŞOCAK <59031493+Kemal-TASOCAK@users.noreply.github.com>
2024-05-02 13:51:19 +0300

    58. Product POST İşlemi

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,7 +12,12 @@ const HomePage = () => {
       try {
         const res = await fetch("http://localhost:5000/api/categories/get-all");
         const data = await res.json();
-        setCategories(data);
+        data &&
+          setCategories(
+            data.map((item) => {
+              return { ...item, value: item.title };
+            })
+          );
       } catch (error) {
         console.log(error);
       }
@@ -25,21 +30,13 @@ const HomePage = () => {
     <>
       <Header />
       <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
-        <div
-          className="categories
-        overflow-auto max-h-[calc(100vh_-_112px)]
-        md:pd-10"
-        >
+        <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
           <Categories categories={categories} setCategories={setCategories} />
         </div>
-        <div
-          className="products flex-[8]
-        max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10
-        "
-        >
-          <Products />
+        <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
+          <Products categories={categories} />
         </div>
-        <div className=" min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
           <CartTotals />
         </div>
       </div>
